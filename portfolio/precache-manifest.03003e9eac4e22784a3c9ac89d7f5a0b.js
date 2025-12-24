138e7f86864b18581b44317556d642e3bd569dad
Mahad Rehan <33506722+mahaaad@users.noreply.github.com>
2025-12-24 00:59:20 -0500

    Restore portfolio files from GitHub Pages

---
+++ portfolio/precache-manifest.03003e9eac4e22784a3c9ac89d7f5a0b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b0e7082bd9ef3746e34a18fc103c541",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "12ca1416bc966af83778",
+    "url": "/website/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "18258d59181bdb5cd061",
+    "url": "/website/static/css/main.c7366372.chunk.css"
+  },
+  {
+    "revision": "12ca1416bc966af83778",
+    "url": "/website/static/js/2.b76de4ec.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/website/static/js/2.b76de4ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18258d59181bdb5cd061",
+    "url": "/website/static/js/main.7ad1ec88.chunk.js"
+  },
+  {
+    "revision": "9c23e00f99dd7916e537",
+    "url": "/website/static/js/runtime-main.9b825fe6.js"
+  }
+]);
---
+++ portfolio/precache-manifest.60941019bb2c9ecf34de4269930c0b06.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "952ba15e27fddd9c34b23b7b2e31ebb3",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "12ca1416bc966af83778",
+    "url": "/website/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "b4863e650b9578438ad3",
+    "url": "/website/static/css/main.3d3a7298.chunk.css"
+  },
+  {
+    "revision": "12ca1416bc966af83778",
+    "url": "/website/static/js/2.b76de4ec.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/website/static/js/2.b76de4ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4863e650b9578438ad3",
+    "url": "/website/static/js/main.f8b1074a.chunk.js"
+  },
+  {
+    "revision": "9c23e00f99dd7916e537",
+    "url": "/website/static/js/runtime-main.9b825fe6.js"
+  }
+]);
